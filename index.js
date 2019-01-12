26b93d881c3771b4e7c1e2d7af4570e7084ebda4
Luca Feigl <sarius009@gmail.com>
2019-01-12 19:41:14 +0100

    Add express server

--- index.js
+++ index.js
@@ -1,46 +1,14 @@
-const _ = require('lodash');
-const Twitter = require('twitter-lite');
-const consumerKey = process.env.TWITTER_CONSUMER_KEY;
-const consumerSecret = process.env.TWITTER_CONSUMER_SECRET;
-let bearerToken = process.env.TWITTER_BEARER_TOKEN;
+const express = require('express');
+const authenticate = require('./authentication.js');
+const port = process.env.TPMD_PORT || 3000;
+const server = express();
 let app = null;
 
-authenticate();
+server.get('/', (req, res) => {
+    res.send('TPMD - Twitter Profile Media Downloader');
+});
 
-async function authenticate() {
-    if (_.isUndefined(bearerToken)) {
-        bearerToken = await getBearerToken();
-    }
-
-    app = new Twitter({
-        bearer_token: bearerToken,
-    });
-}
-
-async function getBearerToken() {
-    const user = new Twitter({
-        consumer_key: consumerKey,
-        consumer_secret: consumerSecret,
-    });
-
-    try {
-        const res = await user.getBearerToken();
-        return res.access_token;
-    } catch (err) {
-        catchError(err);
-    }
-}
-
-function catchError(err) {
-    if ('errors' in err) {
-        err = err.errors[0];
-        console.error(`Twitter API error ${err.code}: ${err.message}`);
-
-        if (err.code === 88) {
-            console.error('Rate limit will reset on', new Date(err._headers['x-rate-limit-reset'] * 1000));
-        }
-    } else {
-        console.error('Non-API error:');
-        console.error(err);
-    }
-}
+server.listen(port, async () => {
+    console.log(`TPMD: Listening on port ${port}.`);
+    app = await authenticate();
+});
