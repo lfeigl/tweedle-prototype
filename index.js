cc1897e0dcfa0cb1e9bf6c3053527de76684127f
Luca Feigl <sarius009@gmail.com>
2019-01-27 19:33:04 +0100

    Refactor index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
-const authenticate = require('./authentication.js');
-const port = process.env.TPMD_PORT || 3000;
+const bodyParser = require('body-parser');
+const authentication = require('./authentication.js');
+const getMediaApi = require('./get-media.js');
+const port = process.env.TPMDL_PORT || 3000;
 const server = express();
-let app = null;
+
+server.use(bodyParser.urlencoded({ extended: true }));
 
 server.get('/', (req, res) => {
-    res.send('TPMD - Twitter Profile Media Downloader');
+    res.send('TPMDL - Twitter Profile Media Downloader');
 });
 
-server.listen(port, async () => {
-    console.log(`TPMD: Listening on port ${port}.`);
-    app = await authenticate();
+server.get('/get', getMediaApi);
+
+server.listen(port, () => {
+    console.log(`TPMDL: Listening on port ${port}.`);
+    authentication.authenticate();
 });
