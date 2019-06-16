fa730e623664dfafa9fe45b229215bf54ad3d36d
Luca Feigl <sarius009@gmail.com>
2019-06-16 23:34:28 +0200

    Fix empty env vars bug

--- scripts/create-dotenv.js
+++ scripts/create-dotenv.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const DOTENV = path.resolve('.env');
 const TEMPLATE = [
     '# Your Twitter app credentials:',
-    'TWITTER_BEARER_TOKEN=<bearer token>',
+    'TWITTER_BEARER_TOKEN=#<bearer token>',
     '# or',
-    'TWITTER_CONSUMER_KEY=<consumer API key>',
-    'TWITTER_CONSUMER_SECRET=<consumer API secret key>',
+    'TWITTER_CONSUMER_KEY=#<consumer API key>',
+    'TWITTER_CONSUMER_SECRET=#<consumer API secret key>',
     '',
     '# TPMDL settings:',
     'TPMDL_PORT=1337',
