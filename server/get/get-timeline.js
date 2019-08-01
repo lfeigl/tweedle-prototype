b0b7131eeac2d1b389de60af4c014b013b6a059a
Luca Feigl <sarius009@gmail.com>
2019-08-01 23:53:35 +0200

    Fix linting errors

--- server/get/get-timeline.js
+++ server/get/get-timeline.js
@@ -1,41 +1,44 @@
+/* eslint no-await-in-loop: 0 */
+
 const _ = require('lodash');
-const authentication = require('../authentication.js');
+const authentication = require('../../utils/authentication.js');
+
 const CONST_PARAMS = {
-    count: 200,
-    trim_user: true,
-    include_rts: false,
-    tweet_mode: 'extended',
+  count: 200,
+  trim_user: true,
+  include_rts: false,
+  tweet_mode: 'extended',
 };
 
-module.exports = async (res, params) => {
-    if (!params.screen_name) return res.sendStatus(400);
+module.exports = async (res, userParams) => {
+  if (!userParams.screen_name) return res.sendStatus(400);
 
-    params = {
-        ...params,
-        ...CONST_PARAMS,
-    };
+  const params = {
+    ...userParams,
+    ...CONST_PARAMS,
+  };
 
-    const app = authentication.getApp();
-    let timeline = [];
-    let timelineChunk = [];
-    let lastId = null;
-    let duplicate = null;
+  const client = authentication.getClient();
+  let timeline = [];
+  let timelineChunk = [];
+  let lastId = null;
+  let duplicate = null;
 
-    do {
-        try {
-            if (lastId) params.max_id = lastId;
-            timelineChunk = await app.get('statuses/user_timeline', params);
-            lastId = _.last(timelineChunk).id_str;
-            timeline = timeline.concat(timelineChunk);
-            duplicate = timeline.pop();
-        } catch (err) {
-            return err;
-        }
-    } while (timelineChunk.length > 1);
+  do {
+    try {
+      if (lastId) params.max_id = lastId;
+      timelineChunk = await client.get('statuses/user_timeline', params);
+      lastId = _.last(timelineChunk).id_str;
+      timeline = timeline.concat(timelineChunk);
+      duplicate = timeline.pop();
+    } catch (err) {
+      return err;
+    }
+  } while (timelineChunk.length > 1);
 
-    res.sendStatus(200);
+  res.sendStatus(200);
 
-    timeline.push(duplicate);
+  timeline.push(duplicate);
 
-    return _.filter(timeline, 'extended_entities').reverse();
+  return _.filter(timeline, 'extended_entities').reverse();
 };
