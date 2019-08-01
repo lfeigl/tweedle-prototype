5b47a1f1a7b1178b7c14f5b5e3416c097e435f86
Luca Feigl <sarius009@gmail.com>
2019-08-01 21:51:17 +0200

    Fix linting errors in scripts directory

--- scripts/create-dotenv.js
+++ scripts/create-dotenv.js
@@ -1,27 +1,28 @@
 const path = require('path');
 const fs = require('fs');
+
 const DOTENV = path.resolve('.env');
 const TEMPLATE = [
-    '# Your Twitter app credentials:',
-    'TWITTER_BEARER_TOKEN=#<bearer token>',
-    '# or',
-    'TWITTER_CONSUMER_KEY=#<consumer API key>',
-    'TWITTER_CONSUMER_SECRET=#<consumer API secret key>',
-    '',
-    '# Tweedle settings:',
-    'TWEEDLE_PORT=1337',
+  '# Your Twitter app credentials:',
+  'TWITTER_BEARER_TOKEN=#<bearer token>',
+  '# or',
+  'TWITTER_CONSUMER_KEY=#<consumer API key>',
+  'TWITTER_CONSUMER_SECRET=#<consumer API secret key>',
+  '',
+  '# Tweedle settings:',
+  'TWEEDLE_PORT=1337',
 ];
 
 module.exports = () => {
-    fs.access(DOTENV, (err) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                fs.writeFile(DOTENV, TEMPLATE.join('\n'), (err) => {
-                    if (err) throw err;
-                });
-            } else {
-                throw err;
-            }
-        }
-    });
+  fs.access(DOTENV, (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.writeFile(DOTENV, TEMPLATE.join('\n'), (error) => {
+          if (error) throw error;
+        });
+      } else {
+        throw err;
+      }
+    }
+  });
 };
--- scripts/create-media-dir.js
+++ scripts/create-media-dir.js
@@ -1,17 +1,18 @@
 const path = require('path');
 const fs = require('fs');
+
 const MEDIA_DIR = path.resolve('media');
 
 module.exports = () => {
-    fs.access(MEDIA_DIR, (err) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                fs.mkdir(MEDIA_DIR, (err) => {
-                    if (err) throw err;
-                });
-            } else {
-                throw err;
-            }
-        }
-    });
+  fs.access(MEDIA_DIR, (err) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        fs.mkdir(MEDIA_DIR, (error) => {
+          if (error) throw error;
+        });
+      } else {
+        throw err;
+      }
+    }
+  });
 };
